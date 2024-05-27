2064cdccb494d393f47f64cd6f99ee1e699a235e
Dangminhnhat <133588149+Dangminhnhat@users.noreply.github.com>
2024-05-27 12:20:38 +0700

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navigation/Navbar'
+import Footer from './components/Navigation/Footer'
 
 export default function App() {
   return (
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   )
 }
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,12 +2,14 @@
 import { Outlet } from 'react-router-dom'
 import '../App.css'
 import Navbar from '../components/Navigation/Navbar'
+import Footer from '../components/Navigation/Footer'
 
 export default function MainLayout() {
     return (
         <>
             <Navbar />
             <Outlet />
+            <Footer />
         </>
     )
 }
