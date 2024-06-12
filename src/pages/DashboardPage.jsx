e90d59f1cf34ffedc0de539ff19f27834adac083
Huu Tri <huutri1572002@gmail.com>
2024-06-12 21:40:17 +0700

    change_password

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const DashboardPage = () => {
     return (
-        <div className='w-full px-4'>
-            HIHI
+        <div className='w-full px-4 text-5xl font-inter font-semibold py-36 text-center'>
+            ADMIN DASHBOARD
         </div>
     );
 }
